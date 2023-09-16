5ce8535ea2d01effc2dec8b4aa9d28cb6f0c5c76
Влад Давиденко <davidenko.vladislaw2017@yandex.ru>
2023-09-16 14:50:47 +0700

    Добавьте файлы проекта.

---
+++ KodXavmana/Model/ParsingLetterModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KodXavmana.ViewModel;
+
+
+namespace KodXavmana.model
+{
+    internal class ParsingLetterModel
+    {
+
+    }
+}
---
+++ KodXavmana/ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodXavmana.ViewModel
+{
+    internal class MainViewModel : BaseViewModel
+    {
+        private string _inputText;
+
+        public string InputText { get => _inputText; set => Set(ref _inputText, value); }
+
+
+        private string _outputText;
+
+        public string OutputText { get => _outputText; set => Set(ref _outputText, value); }
+
+        private string _tableLetter;
+
+        public string TableLetter { get => _tableLetter; set => Set(ref _tableLetter, value); }
+
+
+    }
+}
