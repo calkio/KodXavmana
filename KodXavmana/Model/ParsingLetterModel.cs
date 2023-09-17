0473d2811f23cababa1719254a011df66cf4a04e
Влад Давиденко <davidenko.vladislaw2017@yandex.ru>
2023-09-17 16:10:42 +0700

    Реализация команд + реализация привязок + определение списка вероятностей букв и вывод его на экран

--- KodXavmana/Model/ParsingLetterModel.cs
+++ KodXavmana/Model/ParsingLetterModel.cs
@@ -11,5 +11,33 @@ namespace KodXavmana.model
     internal class ParsingLetterModel
     {
 
+        public static Dictionary<char, double> CalculateLetterProbabilities(string inputText)
+        {
+            Dictionary<char, double> letterCounts = new Dictionary<char, double>();
+            int totalLetters = 0;
+
+            foreach (char c in inputText)
+            {
+                char letter = char.ToLower(c);
+                if (letterCounts.ContainsKey(letter))
+                {
+                    letterCounts[letter]++;
+                }
+                else
+                {
+                    letterCounts[letter] = 1;
+                }
+                totalLetters++;
+            }
+
+            Dictionary<char, double> letterProbabilities = new Dictionary<char, double>();
+            foreach (var kvp in letterCounts)
+            {
+                double probability = kvp.Value / totalLetters;
+                letterProbabilities[kvp.Key] = probability;
+            }
+
+            return letterProbabilities;
+        }
     }
 }
--- KodXavmana/ViewModel/MainViewModel.cs
+++ KodXavmana/ViewModel/MainViewModel.cs
@@ -3,24 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KodXavmana.model;
+using System.Windows.Input;
+using System.Windows;
+using System.Diagnostics;
+using KodXavmana.Infastructure.Commands;
 
 namespace KodXavmana.ViewModel
 {
     internal class MainViewModel : BaseViewModel
     {
+        #region СВОЙСТВА
         private string _inputText;
 
         public string InputText { get => _inputText; set => Set(ref _inputText, value); }
+
 
 
         private string _outputText;
 
         public string OutputText { get => _outputText; set => Set(ref _outputText, value); }
 
+
+
         private string _tableLetter;
 
         public string TableLetter { get => _tableLetter; set => Set(ref _tableLetter, value); }
 
 
+
+        private Dictionary<char, double> _letterProbabilities;
+
+        public Dictionary<char, double> LetterProbabilities { get => _letterProbabilities; set => Set(ref _letterProbabilities, value); }
+
+        #endregion
+
+        public ICommand GetTableLetterProbabilitiesCommand { get; }
+
+        private bool CanGetTableLetterProbabilitiesCommand(object p)
+        {
+            // Проверяем, содержит ли _inputText данные
+            return !string.IsNullOrWhiteSpace(_inputText);
+        }
+
+        private void OnGetTableLetterProbabilitiesCommand(object p)
+        {
+            var dict = ParsingLetterModel.CalculateLetterProbabilities(_inputText);
+
+            TableLetter = string.Join(Environment.NewLine, dict.Select(kv => $"{kv.Key} - {Math.Round(kv.Value, 2)}"));
+        }
+
+        public MainViewModel()
+        {
+            GetTableLetterProbabilitiesCommand = new LambdaCommand(OnGetTableLetterProbabilitiesCommand, CanGetTableLetterProbabilitiesCommand);
+        }
     }
 }
